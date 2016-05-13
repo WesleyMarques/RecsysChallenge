ead2c358be9abaf8559e411b67ac0ea2c11628cb
Wesley <wesley.nmtorres@gmail.com>
2016-05-13 15:29:10 -0300

    relatório checpoint 1

--- test.r
+++ test.r
@@ -1,17 +1,19 @@
-library(SparkR, lib.loc = "/usr/local/spark/R/lib")
-Sys.setenv('SPARKR_SUBMIT_ARGS'='"--packages" "com.databricks:spark-csv_2.11:1.4.0" "sparkr-shell"')
-sc <- sparkR.init(master = "", appName = "SparkR",
-                  sparkHome = "/usr/local/spark", sparkEnvir = list(),
-                  sparkExecutorEnv = list(), sparkJars = "", sparkPackages = "com.databricks:spark-csv_2.11:1.4.0")
-sqlContext <- sparkRSQL.init(sc)
-interactions_data <- read.df(sqlContext, "data/interactions-v01.csv", source = "com.databricks.spark.csv")
-impressions <- read.df(sqlContext, "data/impressions-v02.csv", source = "com.databricks.spark.csv")
-items <- read.df(sqlContext, "data/items_v01_fixed.csv", source = "com.databricks.spark.csv")
-region <- read.df(sqlContext, "data/regions.csv", source = "com.databricks.spark.csv")
-impressions <- read.df(sqlContext, "data/impressions-v02.csv", source = "com.databricks.spark.csv")
-users <- read.df(sqlContext, "data/users_v01_fixed.csv", source = "com.databricks.spark.csv")
+require(ggplot2)
 
+
+interactions_data <- read.csv("data/interactions-v01.csv")
+impressions <- read.cs("data/impressions-v02.csv")
+items <- read.csv("data/items_v01_fixed.csv")
+region <- read.csv("data/regions.csv")
+impressions <- read.csv("data/impressions-v02.csv")
+users <- read.csv("data/users_v01_fixed.csv")
 head(users)
 summary(users)
 head(users)
 
+head(users)
+
+users <- users[ == is.null(users$career_level),]
+plot(users$career_level)
+
+users
